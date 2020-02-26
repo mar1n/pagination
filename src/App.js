2f2ce13c21c4deb2a200042d65c53931837b5d67
mar1n <30738216+mar1n@users.noreply.github.com>
2020-02-26 21:04:18 +0000

    create api request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setpostsPerPage] = useState(1);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My App</h1>
     </div>
   );
 }
---
+++ src/components/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Posts = ({ posts, loading}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default Posts
